3a06aa55a7fb2a4f5cc6aefba668fcbf56d303bf
Uliana Gritchina <ugritchina@icloud.com>
2022-12-14 20:59:38 +0300

    Initial Commit

---
+++ LottieTest/LottieTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieTestApp.swift
+//  LottieTest
+//
+//  Created by Ульяна Гритчина on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LottieTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
